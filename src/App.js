3b9410de102d864997b00ac1339bf3eaae426588
Ssegura09 <stephaniemsegura09@gmail.com>
2021-02-26 13:46:56 -0600

    app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component, Fragment } from 'react';
-import Header from './components/Header'
-import PizzaForm from './components/PizzaForm'
-import PizzaList from './containers/PizzaList'
+import Header from './components/Header';
+import PizzaForm from './components/PizzaForm';
+import PizzaList from './containers/PizzaList';
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <Header />
+        <PizzaForm />
+        <PizzaList />
+        <PizzaIng />
       </Fragment>
     );
   }
